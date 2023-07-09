cf65a03af564f130646a4f476c1cad8c86653047
Ilya <ilya773@vk.com>
2023-07-09 22:48:49 +0400

    ⠀

---
+++ js/main.js
@@ -0,0 +1,27 @@
+import { renderPictures } from './picture.js';
+import { getData, sendData } from './api.js';
+import { showAlert } from './util.js';
+import { setOnFormSubmit, hideModal } from './form.js';
+import { showSuccessMessage, showErrorMessage } from './message.js';
+import { setOnFilterClick, turnFilterOn, filterPictures } from './filter.js';
+
+const onGetDataSuccess = (data) => {
+  turnFilterOn(data);
+  renderPictures(filterPictures());
+  setOnFilterClick(renderPictures);
+};
+
+const onSendDataSuccess = () => {
+  hideModal();
+  showSuccessMessage();
+};
+
+const onSendDataError = () => {
+  showErrorMessage();
+};
+
+setOnFormSubmit(async (data) => {
+  await sendData(onSendDataSuccess, onSendDataError, data);
+});
+
+getData(onGetDataSuccess, showAlert);
